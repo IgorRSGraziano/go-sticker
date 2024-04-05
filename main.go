b6a00a987a8025eaf07e2ac581ee5ed4f2493baf
Igor Graziano <igorrsgraziano@gmail.com>
2024-04-04 23:59:14 -0300

    finally created initial handler

--- main.go
+++ main.go
@@ -10,33 +10,47 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/mdp/qrterminal"
 	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/binary/proto"
 	"go.mau.fi/whatsmeow/store/sqlstore"
 	"go.mau.fi/whatsmeow/types/events"
 	waLog "go.mau.fi/whatsmeow/util/log"
 )
 
-func eventHandler(evt interface{}) {
-	switch v := evt.(type) {
-	case *events.Message:
-		fmt.Println("Received a message!", v.Message.GetConversation())
+// TODO: Passar para um arquivo de configuração
+const (
+	distFolder = "./dist"
+	logLevel   = "WARN"
+)
+
+func getEventHandler(client *whatsmeow.Client) func(evt interface{}) {
+	return func(evt interface{}) {
+		switch v := evt.(type) {
+		case *events.Message:
+			if v.Message.ImageMessage == nil {
+				break
+			}
+			response := "Recebido"
+			client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{
+				Conversation: &response,
+			})
+		}
 	}
 }
 
 func main() {
-	dbLog := waLog.Stdout("Database", "DEBUG", true)
-	// Make sure you add appropriate DB connector imports, e.g. github.com/mattn/go-sqlite3 for SQLite
-	container, err := sqlstore.New("sqlite3", "file:dist/credentials.db?_foreign_keys=on", dbLog)
+	dbLog := waLog.Stdout("Database", logLevel, true)
+	container, err := sqlstore.New("sqlite3", fmt.Sprintf("file:%s/credentials.db?_foreign_keys=on", distFolder), dbLog)
 	if err != nil {
 		panic(err)
 	}
-	// If you want multiple sessions, remember their JIDs and use .GetDevice(jid) or .GetAllDevices() instead.
+
 	deviceStore, err := container.GetFirstDevice()
 	if err != nil {
 		panic(err)
 	}
-	clientLog := waLog.Stdout("Client", "DEBUG", true)
+	clientLog := waLog.Stdout("Client", logLevel, true)
 	client := whatsmeow.NewClient(deviceStore, clientLog)
-	client.AddEventHandler(eventHandler)
+	client.AddEventHandler(getEventHandler(client))
 
 	if client.Store.ID == nil {
 		// No ID stored, new login
@@ -47,16 +61,12 @@ func main() {
 		}
 		for evt := range qrChan {
 			if evt.Event == "code" {
-				// Render the QR code here
-				// e.g. qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
-				// or just manually `echo 2@... | qrencode -t ansiutf8` in a terminal
 				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
 			} else {
 				fmt.Println("Login event:", evt.Event)
 			}
 		}
 	} else {
-		// Already logged in, just connect
 		err = client.Connect()
 		if err != nil {
 			panic(err)
